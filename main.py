e18acf0ea2daef65e5912fa7ce54ce2b9054c370
Shreyoshi Deb <shreyoshideb0@gmail.com>
2025-09-16 21:07:53 -0400

    fixed cors issue

--- main.py
+++ main.py
@@ -9,20 +9,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 # Initialize FastAPI
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app = FastAPI(
     title="healthsumai-backend",
     description="API Docs",
     version="1.0.0"  
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://health-sum-ai.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 #Language Credentials
